a4e9f211046aa8a9da0217d2f7a66f6e7d6fb22a
Jerry Ejonavi <jerryasih@gmail.com>
2019-09-06 02:16:20 +0100

    add app constants

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Typography } from "antd";
+import { APP_NAME } from "../../utils/app-constants";
+
+const { Title } = Typography;
 
 class Home extends Component {
     public render(): JSX.Element {
         return (
             <div className="homePage">
-                <h1>Hello World!</h1>
+                <Title level={1}>{APP_NAME}</Title>
+                <p>Welcome to {APP_NAME}. If you have saved some contacts they should appear here.</p>
             </div>
         );
     }
