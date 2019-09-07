dd9ca219b520a36930b756c428b663d5544c455b
Jerry Ejonavi <jerryasih@gmail.com>
2019-09-07 17:43:55 +0100

    add interfaces for Contact and app State

--- src/interfaces/Contact.ts
+++ src/interfaces/Contact.ts
@@ -1,7 +1,9 @@
 export interface Contact {
+    [x: string]: string | number | undefined | string[];
+    id?: number;
     name: string;
     birthday: string;
     phone: string;
     email: string;
-    additionalEmails: string[];
+    additionalEmails?: string[];
 }
